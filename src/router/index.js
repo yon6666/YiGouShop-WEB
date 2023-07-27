235206188e9bfd727f3c6a5ed7f4f284e685b16e
haoyuan <lihaoyuan0314@163.com>
2023-07-27 14:45:22 +0800

    上传目录

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = createRouter({
+  // 使用hash方式实现路由
   history: createWebHashHistory(),
+  // 配置路由规则，写法和之前一样
   routes
 })
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
 import { createStore } from 'vuex'
 
+// export default createStore({
+//   state: {
+//     username: 'xiaotuxian'
+//   },
+//   mutations: {
+//   },
+//   actions: {
+//   },
+//   modules: {
+//   }
+// })
+const moduleA = {
+
+}
+
+const moduleB = {
+
+}
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  moduleA, moduleB
 })
