7bf34cc6bb88dbcd0c643a5b7bd0c8dd8f1ce3e2
yon <lihaoyuan0314@163.com>
2023-08-10 23:09:28 +0800

    Update index.vue

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,18 +1,98 @@
 <template>
     <div class="top-category">
-        <div class="container">
-            <XtxBread>
-                <XtxBreadItem :to="{path:'/'}">首页</XtxBreadItem>
-                <XtxBreadItem to="/category/1005000">电器</XtxBreadItem>
-                <XtxBreadItem>空调</XtxBreadItem>
-            </XtxBread>
-     </div>
+      <div class="container">
+        <!-- 面包屑 -->
+        <XtxBread>
+          <XtxBreadItem to="/">首页</XtxBreadItem>
+          <XtxBreadItem>{{topCategory.name}}</XtxBreadItem>
+        </XtxBread>
+        <!-- 轮播图 -->
+        <XtxCarousel :sliders="sliders" style="height:500px" />
+        <!-- 所有二级分类 -->
+        <div class="sub-list">
+          <h3>全部分类</h3>
+          <ul>
+            <li v-for="item in topCategory.children" :key="item.id">
+              <a href="javascript:;">
+                <img :src="item.picture" >
+                <p>{{item.name}}</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- 不同分类商品 -->
+      </div>
     </div>
-</template>
+  </template>
 <script>
-
+import { findBanner } from '@/api/home'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+import { computed, ref } from 'vue'
 export default {
-    name: 'TopCategory'
+
+    name: 'TopCategory',
+
+    setup () {
+        const sliders = ref([])
+        findBanner().then(data => {
+            sliders.value = data.result
+        })
+
+        const store = useStore()
+        const route = useRoute()
+            const topCategory = computed(() => {
+                let cate = {}
+                const item = store.state.category.list.find(item => {
+        return item.id === route.params.id
+            })
+      if (item) cate = item
+      return cate
+        })
+
+        return {
+            sliders, topCategory
+        }
+    }
+
 }
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+      li {
+        width: 168px;
+        height: 160px;
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+          img {
+            width: 100px;
+            height: 100px;
+          }
+          p {
+            line-height: 40px;
+          }
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
