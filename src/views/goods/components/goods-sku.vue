5a0037485506c1b0fece82828b4c5e46f19ec283
haoyuan <lihaoyuan0314@163.com>
2023-08-18 17:22:09 +0800

    1
    2

--- src/views/goods/components/goods-sku.vue
+++ src/views/goods/components/goods-sku.vue
@@ -4,14 +4,58 @@
         <dt>{{item.name}}</dt>
         <dd>
           <template v-for="val in item.values" :key="val.name">
-            <img  :class="{selected:val.selected}" @click="clickSpecs(item,val)" v-if="val.picture" :src="val.picture" :title="val.name">
-            <span :class="{selected:val.selected}" @click="clickSpecs(item,val)" v-else>{{val.name}}</span>
+            <img  :class="{selected:val.selected,disabled:val.disabled}" @click="clickSpecs(item,val)" v-if="val.picture" :src="val.picture" :title="val.name">
+            <span :class="{selected:val.selected,disabled:val.disabled}" @click="clickSpecs(item,val)" v-else>{{val.name}}</span>
           </template>
         </dd>
       </dl>
     </div>
   </template>
   <script>
+  import getPowerSet from '@/vender/power-set'
+  const spliter = '★'
+  const getPathMap = (skus) => {
+    const pathMap = {}
+        skus.forEach(
+          sku => {
+            if (sku.inventory > 0) {
+              const specs = sku.specs.map(val => val.valueName)
+              const powerSet = getPowerSet(specs)
+              powerSet.forEach(arr => {
+                const key = arr.join(spliter)
+                  if (pathMap[key]) {
+                  // 已经有key往数组追加
+                  pathMap[key].push(sku.id)
+                } else {
+                  // 没有key设置一个数组
+                  pathMap[key] = [sku.id]
+                }
+              })
+            }
+          }
+        )
+        return pathMap
+  }
+const updateDisabledStatus = (specs, pathMap) => {
+  specs.forEach((item, i) => {
+    const selectedValues = getSelectedArr(specs)
+    item.values.forEach(val => {
+      if (val.selected) return
+      selectedValues[i] = val.name
+      const key = selectedValues.filter(value => value).join(spliter)
+      val.disabled = !pathMap[key]
+   })
+})
+}
+
+const getSelectedArr = (specs) => {
+  const selectedArr = []
+  specs.forEach(spec => {
+    const selectedVal = spec.values.find(val => val.selected)
+    selectedArr.push(selectedVal ? selectedVal.name : undefined)
+  })
+  return selectedArr
+}
   export default {
     name: 'GoodsSku',
     props: {
@@ -21,7 +65,13 @@
     }
   },
   setup (props) {
+    const pathMap = getPathMap(props.goods.skus)
+    console.log(pathMap)
+    updateDisabledStatus(props.goods.specs, pathMap)
     const clickSpecs = (item, val) => {
+      if (val.disabled) {
+        return
+      }
       // 1. 选中与取消选中逻辑
       if (val.selected) {
         val.selected = false
