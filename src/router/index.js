47b7031d8ab1d299413fcad0c232e6e51b1f4587
haoyuan <lihaoyuan0314@163.com>
2023-08-21 17:20:22 +0800

    登录
    111

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import SubCategory from '@/views/category/sub'
 const Layout = () => import('@/views/Layout')
 const Home = () => import('@/views/home')
 const Goods = () => import('@/views/goods/index')
+const Login = () => import('@/views/login/index')
 const routes = [
   {
     path: '/',
@@ -14,7 +15,8 @@ const routes = [
     { path: '/category/sub/:id', component: SubCategory },
     { path: '/product/:id', component: Goods }
     ]
-}
+},
+{ path: '/login', component: Login }
 ]
 
 const router = createRouter({
